ad8bb06acaa1a75f36cfe239345ebc97f2a9d5a0
Sai4Nandhan <sainandhan2212@gmail.com>
2026-03-11 21:05:13 +0530

    First backend with student and faculty api's

---
+++ Server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const StudentRoutes = require("./routes/studentRoutes");
+const FacultyRoutes = require("./routes/facultyRoutes");
+
+const app = express();
+
+app.use(express.json());
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/studentDB")
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+app.use("/api", StudentRoutes);
+app.use("/api", FacultyRoutes);
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
